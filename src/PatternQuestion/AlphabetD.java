1e9acb53773e02e8a87729e873faf1548f4c97c4
sakshi1feb <sakshi1feb@gmail.com>
2023-05-26 22:36:36 +0530

    created a pattern for displaying the character D

--- src/PatternQuestion/AlphabetD.java
+++ src/PatternQuestion/AlphabetD.java
@@ -2,14 +2,14 @@ package PatternQuestion;
 
 import java.util.Scanner;
 
-public class AlphabetB {
+public class AlphabetD {
     public static void main(String[] args) {
-        Scanner sc=new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter value of N");
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==0 || i==0 && j<n-1 || i==(n-1)/2 && j<n-1 || i==(n-1)&& j<n-1 || j==n-1 && i>0 && i!=(n-1)/2 && i<n-1)
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || i == 0 && j < n - 1 || i == n - 1 && j < n - 1 || j == n - 1 && i > 0 && i < n - 1)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
