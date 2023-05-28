8c546403631ebf93f5bc964a73d0f1c604ed5110
sakshi1feb <sakshi1feb@gmail.com>
2023-05-28 11:50:54 +0530

    Code for displaying Alphabet G

---
+++ src/PatternQuestion/AlphabetG.java
@@ -0,0 +1,21 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class AlphabetG {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter value of N:");
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                    if(i==0 && j>0 && j<n-1 || j==0&& j>0 && j<n-1 || i==n-1 && j>0 && j<n-1|| i==(n-1)/2 && j>=(n-1)/2
+                        || i==(n-2) && j==n-1 || i==1 && j==(n-1) || j==0 && i>0 && i<n-1)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
