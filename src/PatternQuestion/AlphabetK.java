f0264ed97ef327a404beffe30aba4990ed942f05
sakshi1feb <sakshi1feb@gmail.com>
2023-05-28 12:15:49 +0530

    Code for displaying alphabet K

---
+++ src/PatternQuestion/AlphabetK.java
@@ -0,0 +1,20 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class AlphabetK {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of N:");
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j==0 ||  i+j==(n-1)/2 || i-j==(n-1)/2)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
