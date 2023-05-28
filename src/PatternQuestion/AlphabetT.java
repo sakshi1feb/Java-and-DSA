4828389c634a7d58e2d88a7b3e054f2eee64a235
sakshi1feb <sakshi1feb@gmail.com>
2023-05-28 13:30:46 +0530

    program to display the character T

---
+++ src/PatternQuestion/AlphabetT.java
@@ -0,0 +1,20 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class AlphabetT {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of N: ");
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0 || j==(n-1)/2)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
