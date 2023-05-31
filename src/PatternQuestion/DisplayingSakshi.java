18df95cb6d81a1fed65db0bd1fdd103b13e36c9f
sakshi1feb <sakshi1feb@gmail.com>
2023-05-31 16:30:47 +0530

    Program to display a pattern to print your name

---
+++ src/PatternQuestion/DisplayingSakshi.java
@@ -0,0 +1,59 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class DisplayingSakshi {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);{
+            System.out.println("Enter value of N :");
+            int n=sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(i==0 && j>0 && j<n-1 || i==n-1 && j>0 && j<n-1 || i==(n-1)/2 && j>0 && j<n-1 || j==0 && i>0 && i<(n-1)/2
+                            || j==n-1 && i>(n-1)/2 && i<n-1 || j==n-1 && i==1 || j==0 && i==n-2)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.print("  ");
+                for(int j=0;j<n;j++){
+                    if(i==(n-1)/2 || j==0 && i>0|| j==(n-1) && i>0 || i==0 && j>0 && j<n-1)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.print("  ");
+                for (int j = 0; j < n; j++) {
+                    if(j==0 ||  i+j==(n-1)/2 || i-j==(n-1)/2)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    if(i==0 && j>0 && j<n-1 || i==n-1 && j>0 && j<n-1 || i==(n-1)/2 && j>0 && j<n-1 || j==0 && i>0 && i<(n-1)/2
+                            || j==n-1 && i>(n-1)/2 && i<n-1 || j==n-1 && i==1 || j==0 && i==n-2)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.print("  ");
+                for(int j=0;j<n;j++){
+                    if(j==0||j==(n-1)||i==(n-1)/2)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+
+                }
+                System.out.print("  ");
+                for (int j=0;j<n;j++){
+                    if(i==0 || i==n-1 || j==(n-1)/2)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
