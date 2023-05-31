9a77269798c3271a9f480e248230c69ffbf5e997
sakshi1feb <sakshi1feb@gmail.com>
2023-05-31 19:20:18 +0530

    kk pattern problem 9

---
+++ src/PatternQuestion/kk9.java
@@ -0,0 +1,27 @@
+package PatternQuestion;
+/**
+ *     *********
+ *      *******
+ *       *****
+ *        ***
+ *         *
+ * */
+import java.util.Scanner;
+
+public class kk9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of N ");
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i-j<=0 && i<=(n-1)/2 && j<=(n-1)/2 ||  i+j<=n-1 && i<=(n-1)/2 && j>=(n-1)/2 ) {
+                    System.out.print("* ");
+                } else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+        }
+    }
+}
