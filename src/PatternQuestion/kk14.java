5f6f6759a7c298d29934ab69d6d08d6f225d7913
sakshi1feb <sakshi1feb@gmail.com>
2023-06-01 18:36:49 +0530

    kk pattern question 14

---
+++ src/PatternQuestion/kk14.java
@@ -0,0 +1,21 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class kk14 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of N ");
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0 || i-j==0 && i<=(n-1)/2 || i+j==n-1 && j>=(n-1)/2 ) {
+                    System.out.print("* ");
+                } else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+        }
+    }
+}
