fc08d1b7330a050a96860aa2842d7d80c00da84f
sakshi1feb <sakshi1feb@gmail.com>
2023-06-03 00:54:14 +0530

    modified the problem solving technique

--- src/PatternQuestion/kk5.java
+++ src/PatternQuestion/kk5.java
@@ -7,13 +7,19 @@ public class kk5 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter value of N");
         int n=sc.nextInt();
-        for (int i = 0; i < n; i++) {
+        /*for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if(j==0 || i-j>=0 && i<=(n-1)/2|| i+j<=n-1 && i>=(n-1)/2 )
                     System.out.print("* ");
                 else
                     System.out.print("  ");
             }
+            System.out.println();
+        }*/
+        for (int i = 0; i <2*n; i++) {
+            int c=i>n?2*n-i:i;
+            for(int j=0;j<c;j++)
+                System.out.print("* ");
             System.out.println();
         }
     }
