472228a0c9afe652a78f289aaac58b7c2a4d0f8a
sakshi1feb <sakshi1feb@gmail.com>
2023-06-05 00:31:36 +0530

    kk pattern problem 32

---
+++ src/PatternQuestion/Pattern32.java
@@ -0,0 +1,22 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class Pattern32 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of N:");
+        int n=sc.nextInt();
+        n=64+n;
+        for (int r =n; r>=65; r--) {
+            {
+                for (int c =r; c<=n ; c++) {
+                    System.out.print((char)c +" ");
+                }
+
+                }
+            System.out.println();
+        }
+
+    }
+}
