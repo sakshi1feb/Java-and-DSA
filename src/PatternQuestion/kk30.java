371ce89d3ce7b098a7a724c992fec56920aeb17d
sakshi1feb <sakshi1feb@gmail.com>
2023-06-04 22:55:49 +0530

    kk pattern problem 30

---
+++ src/PatternQuestion/kk30.java
@@ -0,0 +1,21 @@
+package PatternQuestion;
+
+import java.util.Scanner;
+
+public class kk30 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of N:");
+        int n=sc.nextInt();
+        for (int r = 1; r <= n; r++) {
+            for (int s=0;s<=n-r;s++)
+                System.out.print("  ");
+            for(int col=r;col>=1;col--)
+                System.out.print(col +" ");
+            for(int col=2;col<=r;col++)
+                System.out.print(col+" ");
+            System.out.println();
+        }
+
+    }
+}
