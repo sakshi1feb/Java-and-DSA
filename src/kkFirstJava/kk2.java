284c965d7804dea690a48b17151cc16c752246a4
sakshi1feb <sakshi1feb@gmail.com>
2023-06-10 00:21:14 +0530

    kk first java problem 2

---
+++ src/kkFirstJava/kk2.java
@@ -0,0 +1,13 @@
+package kkFirstJava;
+
+import java.util.Scanner;
+
+public class kk2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String name=sc.next();
+        System.out.println("Hello "+name+" 😊 Nice to meet you");
+
+    }
+}
